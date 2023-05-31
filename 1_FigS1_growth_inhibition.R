2f3b8ef141ffe436eb91b6040f424824b57e8198
drozdovapb <drozdovapb@gmail.com>
2023-05-31 10:38:20 +0800

    growth inhibition comment out unused

--- 1_FigS1_growth_inhibition.R
+++ 1_FigS1_growth_inhibition.R
@@ -2,40 +2,40 @@ library(openxlsx)
 library(ggplot2)
 library(ggbeeswarm)
 
-dat <- read.xlsx("LA_yeast_growth.xlsx") ## rename S...
+dat <- read.xlsx("data/LA_yeast_growth.xlsx") ## rename S...
 
 ## doubling time doesn't work
-ggplot(dat, aes(x=condition, y=Doubling.time)) + 
-  geom_point() + ylim(0,10)
+#ggplot(dat, aes(x=condition, y=Doubling.time)) + 
+#  geom_point() + ylim(0,10)
 
 #a <- 
-ggplot(dat, aes(x=condition, y=Log2.Growth, col = condition)) + 
-  geom_boxplot() + 
-  geom_beeswarm() + 
-  #geom_jitter(width =.1) + 
-  xlab("") + ylab("log2 growth") + 
-  theme_bw() + 
-  scale_x_discrete(labels = c("control", "50 uM DLA", "500 uM DLA", "5 mM DLA", "45 mM DLA", "45 mM DLA")) + 
-  scale_color_manual(values = c("black", blues9[5:8], "yellow4"))
-#ggsave("growth_inhibition.png", width = 20, height = 10, units="cm")
+#ggplot(dat, aes(x=condition, y=Log2.Growth, col = condition)) + 
+#  geom_boxplot() + 
+#  geom_beeswarm() + 
+#  #geom_jitter(width =.1) + 
+#  xlab("") + ylab("log2 growth") + 
+#  theme_bw() + 
+#  scale_x_discrete(labels = c("control", "0.05 mM DLA", "0.5 mM DLA", "5 mM DLA", "45 mM DLA", "45 mM DLA")) + 
+#  scale_color_manual(values = c("black", blues9[5:8], "yellow4"))
+##ggsave("growth_inhibition.png", width = 20, height = 10, units="cm")
 
 pairwise.wilcox.test(dat$Log2.Growth, dat$condition)
 
 
 ## relative growth rate!
 
-#b <- 
+png("figs/FigS1_growth_inhibition.png", width = 20, height = 8, units="cm", res=200)
 ggplot(dat, aes(x=condition, y=RGR, col = condition)) + 
   geom_boxplot() + 
   geom_beeswarm() + 
   #geom_jitter(width =.1) + 
   xlab("") + ylab("Relative growth rate") + 
   theme_bw() + 
-  scale_x_discrete(labels = c("control", "50 uM DLA", "500 uM DLA", "5 mM DLA", "45 mM DLA", "45 mM LLA")) + 
+  scale_x_discrete(labels = c("control", "0.05 mM DLA", "0.5 mM DLA", "5 mM DLA", "45 mM DLA", "45 mM DLA")) + 
   scale_color_manual(values = c("black", blues9[5:8], "yellow4")) + 
   theme(legend.position = 'none')
-ggsave("growth_inhibition.png", width = 20, height = 8, units="cm")
-
+#ggsave("figs/FigS1_growth_inhibition.png", width = 20, height = 8, units="cm")
+dev.off()
 
 pairwise.wilcox.test(dat$RGR, dat$condition, p.adjust.method = "BH")
 library(dunn.test)
